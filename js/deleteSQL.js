e20f207337db481e410154325dc421d2fbdfa9ef
lpenaud <l.penaud@zaclys.net>
2017-04-06 22:09:33 +0000

    Ajout fonction supprSQL()

--- js/deleteSQL.js
+++ js/deleteSQL.js
@@ -1,22 +1,5 @@
 $(document).ready(function () {
     $("img[alt='supprimer']").click(function () {
-        if (confirm("Désirez-vous vraiment supprimer cette utilisateur ?")) {
-            var id = $("img[alt='supprimer']").attr('id').substr(5);
-            console.log(id);
-            $("table").fadeOut("slow");
-            $("#chargement").fadeIn("slow")
-            $.ajax({
-                url: "/ajax/deleteTable.php",
-                type: "POST",
-                data: {id: id, table: $("table").attr('id')},
-                success: function(data) {
-                    console.log(JSON.stringify(data));
-                    $('#tr'+id).hide();
-                    $('#chargement').fadeOut("slow")
-                    $("h5.invisible").fadeIn("slow")
-                    $("table").fadeIn("slow");
-                },
-            });
-        }
+        deleteSQL(this, "Désirez-vous vraiment supprimer cette utilisateur ?");
     });
 });
